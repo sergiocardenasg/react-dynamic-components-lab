7b3fca93835b93be5ba82daeccdcb88b7cb7b5f1
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-14 08:29:54 -0500

    Revert "solved"
    This reverts commit 7afb880557772afaf75bfb37085daa3c60fa9f52.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Comment from './Comment.js'
 // are we missing an import?
 
 export default class BlogPost extends Component {
@@ -19,11 +18,11 @@ export default class BlogPost extends Component {
         Still I'll rise.<br/>
         -Maya Angelou<br/>
 
-        
+        {/* (remove the comment ticks and their surrounding brackets)
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[1]} />
-        <Comment commentText={comments[2]} />
-        
+        <Comment commentText={comments[0]} />
+        <Comment commentText={comments[0]} />
+        */}
       </div>
     );
   }
